c878e299a4d82b3b906ca199dfac2e2e8936d107
Matt Anderson <mthwanderson20@gmail.com>
2026-07-07 14:38:43 -0500

    Initial Commit

---
+++ DashboardOps/DashboardOpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardOpsApp.swift
+//  DashboardOps
+//
+//  Created by Mathew Anderson on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct DashboardOpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DashboardOpsTests/DashboardOpsTests.swift
@@ -0,0 +1,19 @@
+//
+//  DashboardOpsTests.swift
+//  DashboardOpsTests
+//
+//  Created by Mathew Anderson on 7/7/26.
+//
+
+import Testing
+@testable import DashboardOps
+
+struct DashboardOpsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
